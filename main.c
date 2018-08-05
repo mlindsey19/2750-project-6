d3ee1c5ae77e68b454687caacdccde799620edb5
Mitch Lindsey <dmld54@mail.umsl.edu>
2018-08-05 12:33:34 -0500

    done

--- main.c
+++ main.c
@@ -8,7 +8,11 @@
 #include "menu.h"
 #include "struct.h"
 #include "getAccts.h"
+#include <locale.h>
+#include <string.h>
+#include <math.h>
 
+long int validAcctNum();
 int checkForAcctNum(long int const *, long int);
 void writeToFile(FILE *, acctInfo *, const int);
 void deposit(acctInfo *,const long double);
@@ -19,7 +23,6 @@ void removeAcct(const int,const int , long int *, FILE *);
 	
 
 int main( int argc, char **argv ) {
-	
 	
 	FILE *acct_fptr = fopen( "accounts.dat", "rb+" ); //accounts.dat file pointer
 	if ( acct_fptr ==  NULL) 
@@ -39,76 +42,68 @@ int main( int argc, char **argv ) {
 	
 	numberOfAccts = getAccts(acct_fptr, acctNumArray);
 
-	for ( int i = 0; i < 9; i++)
-		printf("%li ", acctNumArray[i]);
-	
-	printf("\n\t\t\t\tnumber of accts:%i\n\n", numberOfAccts);
-
-	while (numberOfAccts < 50 ){
+	while ( 1 ){
 	switch ( menu() ){
 
 		case 0:
-			fclose(acct_fptr);
-			return 0;
+			fclose(acct_fptr);		return 0;
 		case 1: 
 			//deposit
 			printf("enter account number:\n");
-			scanf("%li", &number);
+			number = validAcctNum();
 			int p =	checkForAcctNum(acctNumArray, number);
 			if (p != -1){
 				fseek(acct_fptr, p * sizeof(acctInfo), SEEK_SET);
 				fread(user_ptr, sizeof(acctInfo), 1, acct_fptr);
-				deposit(user_ptr, value);
-				scanf ("%Lf", &value);
+				fseek(acct_fptr, p * sizeof(acctInfo), SEEK_SET);
 				printf("enter ammount : $");
+ 				scanf ("%Lf", &value);
+				deposit(user_ptr, value);
 				writeToFile(acct_fptr, user_ptr, p);
 			}			
 			break;
 		case 2:
 			//withdrawal	
 			printf("enter account number:\n");
-			scanf("%li", &number);
+			number = validAcctNum();
 			int k =	checkForAcctNum(acctNumArray, number);
 			if (k != -1){
 				fseek(acct_fptr, k * sizeof(acctInfo), SEEK_SET);
 				fread(user_ptr, sizeof(acctInfo), 1, acct_fptr);
+				fseek(acct_fptr, k * sizeof(acctInfo), SEEK_SET);
 				printf("enter ammount : $");
 				scanf ("%Lf", &value);
 				withdrawal(user_ptr, value);
-				writeToFile(acct_fptr, user_ptr, p);
+				writeToFile(acct_fptr, user_ptr, k);
 			}
 			break;
 		case 3: 
 			newAcct(&user);
+			if (user.acctNum == -1 ){
+				break;
+			}
 			acctNumArray[numberOfAccts] = user.acctNum;
-			for (int i = 0; i < 5; i++){
-				printf("\t%i:   %li\n", i,  acctNumArray[i]);
-				}
-
 			writeToFile(acct_fptr, &user, numberOfAccts);
-			readFile(acct_fptr, numberOfAccts );
 			numberOfAccts++;
 			break;
 		case 4:
 			printf("enter account number:\n");
-			scanf("%li", &number);
+			number = validAcctNum();
 			int m =	checkForAcctNum(acctNumArray, number);
 			if (m != -1){
 				//remove account
 			removeAcct( m, numberOfAccts, acctNumArray, acct_fptr );
 			numberOfAccts--;
 			}
-
 			break;
 		case 5: 
 			//ballance inquiry
 			printf("enter account number:\n");
-			scanf("%li", &number);
+			number = validAcctNum();
 			int s =	checkForAcctNum(acctNumArray, number);
 			fseek(acct_fptr, s * sizeof(acctInfo), SEEK_SET);
 			fread(user_ptr, sizeof(acctInfo), 1, acct_fptr);
 			printf("account balance is: \t%.2Lf", user_ptr->acctBal);
-
 			break;
 		case 6: 
 			
@@ -119,7 +114,7 @@ int main( int argc, char **argv ) {
 				fseek(acct_fptr, i * sizeof(acctInfo), SEEK_SET);
 				fread(user_ptr, sizeof(acctInfo), 1, acct_fptr);
 				displayAcct(user_ptr);	
-				}			
+			}			
 			break;
 		default:
 			break;
@@ -130,10 +125,26 @@ int main( int argc, char **argv ) {
 
 return 0;
 }
+long int validAcctNum(){
+	char numstr [55];
+	long int num;
+	
+	scanf("%54s", numstr);
+	int x = strlen(numstr);
+
+	if (x == 6){
+		num = atol(&numstr[0]);      
+		return num;
+	}
+	printf("\n\t*** Expected 6 digit number, starting with '1-9' ***\n");
+	return -1;
+}
+
 	//print to file
 void writeToFile(FILE *acct_fptr, acctInfo *user, const int N){		
 	fseek( acct_fptr, (N * sizeof(acctInfo)), SEEK_SET );		
 	fwrite(user, sizeof(acctInfo), 1, acct_fptr);
+	rewind(acct_fptr);
 }
 
 void readFile(FILE *acct_fptr, const int N){
@@ -141,6 +152,7 @@ void readFile(FILE *acct_fptr, const int N){
 	fseek(acct_fptr, ( (N) * sizeof(acctInfo)), SEEK_SET);
 	fread(&userTest, sizeof(acctInfo), 1, acct_fptr);
 	displayAcct(&userTest);
+	rewind(acct_fptr);
 }
 int checkForAcctNum(long int const *arr, long int x){
 	int position = -1;
@@ -154,15 +166,16 @@ void deposit (acctInfo * user, const long double X){
 	user->acctBal += X;
 }
 void withdrawal (acctInfo * user, const long double X){
-	if (user->acctBal > X){
+	if (user->acctBal >= X){
 	user->acctBal -= X;
 	} else {
 	printf("not enought funds");
 	}	
 }
 void displayAcct(acctInfo *user){
-	printf("\t%s %s %s \naccount number: %li\nbalance:%.2Lf", user->firstName, user->middleI, 
-			user->lastName, user->acctNum, user->acctBal);
+	setlocale( LC_NUMERIC, "en_US" );
+
+	printf("--------------------------------------------------------\n\tName:\t%s %s %s \naccount number: %li\nbalance: $%'.2Lf\n--------------------------------------------------------\n", user->firstName, user->middleI, user->lastName, user->acctNum, user->acctBal);
 
 }
 
@@ -182,11 +195,8 @@ void removeAcct(const int m,const int num, long int *acctNumArray, FILE *acct_fp
 		} 
 	}
 	for ( int i = 0; i < 50; i++ ) {
-		acctNumArray[i] = temparr[i];
-		
+		acctNumArray[i] = temparr[i];	
 	}
-
-
 	FILE *tmpFP = fopen( "temp.dat", "wb" ); //accounts.dat file pointer
 	if ( tmpFP ==  NULL) 
 		{
@@ -214,6 +224,5 @@ void removeAcct(const int m,const int num, long int *acctNumArray, FILE *acct_fp
 		{
 		  printf( "ERROR: the File accounts.dat could not be opened!\n");
 		}
-
 
 }
--- newAcct.c
+++ newAcct.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include "struct.h"
 #include <string.h>
+#include <math.h>
 
 void newAcct(acctInfo *user){
 
@@ -22,9 +23,32 @@ void newAcct(acctInfo *user){
 	scanf( "%31s", user->lastName);
 
 	printf( "Choose 6 digit account number: \n" );
-	scanf( "%li", &(user->acctNum) );
 
+	char numstr [55];
+	long int num = 0;
+	
+	scanf("%54s", numstr);
+	int x = strlen(numstr);
+
+	if (x == 6){
+
+		
+		num += atol(&numstr[0]);
+	//	num += atol(&numstr[1]) * 10; 
+		
+	//	num += atol(&numstr[2]) * 100;
+	//	num += atol(&numstr[3]) * 1000;
+	//	num += atol(&numstr[4]) * 100000;
+	//	num += atol(&numstr[5]) * 1000000;
+		
+	user->acctNum = num;
 	printf( "Enter account balance: \n" );
 	scanf( "%Lf", &(user->acctBal) );
+	}
+	else {
+		user->acctNum = -1;
+	printf("\n\t*** Expected 6 digit number, starting with '1-9' ***\n");
+	}
+	
 }
 
